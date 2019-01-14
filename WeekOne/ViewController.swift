d69c48aaf046d0220830c673ad4b4d95cec33b88
John Gallaugher <john.gallaugher@bc.edu>
2019-01-14 17:58:10 -0500

    Initial Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by John Gallaugher on 1/14/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
